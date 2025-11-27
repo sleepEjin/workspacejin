14f22094be73a4f9a96ef704b37a9c121d66fc06
sleepEjin <jinking0930@naver.com>
2025-11-27 12:35:53 +0900

    .

--- 06_React/react-project/src/App.jsx
+++ 06_React/react-project/src/App.jsx
@@ -4,30 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Profile from './pages/Profile'
 import NotFound from './pages/NotFound'
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
-//BrowserRouter : 전체 라우팅을 관리하는 최상위 컴포넌트 -> URL 변화를 추적해서 그에 맞는 페이지를 렌더링
-//Routes : Route들을 감싸서 경로를 비교하고 그중 하나를 렌더링 시키는 라우트 목록
-//Route : path와 elements로 한 페이지에 대한 규칙을 적용하는 컴포넌트
-//Link : a 태그와 동일한 역할을 하지만 실제 서버에 요청을 보내지 않고 SPA 방식으로 브라우저 단에서 react-router-dom을 통한 이동
+// BrowserRouter : 전체 라우팅을 관리하는 최상위 컴포넌트 -> URL변화를 추적해서 그에맞는 페지이를 렌더링
+// Routes : Route들을 감싸서 경로를 비교하고 그중 하나를 렌더링 시키는 라우트 목록
+// Route : path와 elements로 한 페이지에 대한 규칙을 정의하는 컴포넌트
+// Link: a태그와 동일한 역할을 하지만 실제 서버에 요청을 보내지않고 SPA방식으로 브라우저단에서 react-router-dom을 통한 이동
 
 function App() {
 
   return (
     <BrowserRouter>
-    <nav>
-      <Link to="/">홈</Link>
-      <Link to="/about">소개</Link>
-      <Link to="/profile">프로필</Link>
-    </nav>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />}/>
-      <Route path='/profile/:username' element={<Profile />}/>
-      <Route path='*' element={<NotFound />}/>
-    </Routes>
+      <nav>
+        <Link to="/">홈</Link>
+        <Link to="/about">소개</Link>
+        <Link to="/profile">프로필</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile/:username' element={<Profile />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
     </BrowserRouter>
   )
 }
--- 06_React/react-project/src/pages/Profile.jsx
+++ 06_React/react-project/src/pages/Profile.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom'
 
 const Profile = () => {
     const {username} = useParams();
-    
-    const {search} = useLocation();
+
+    const {search} = useLocation(); //모든 쿼리스트림
+    console.log(search);
     const query = new URLSearchParams(search);
 
-    const sort=query.get('sort');
-    console.log(sort);
-
+    const sort = query.get('sort');
+    console.log(query);
     return (
-        <>
-    <div>프로필페이지입니다</div>
-    <p>{username}의 프로필</p>
+    <>
+        {sort}
+        <div>프로필페이지 입니다.</div>
+        <p>{username}의 프로필</p>
     </>
   )
 }
