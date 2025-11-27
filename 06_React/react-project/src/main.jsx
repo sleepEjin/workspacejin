732dfd5471554fdd1baa23cb7b305bb5598e3ac1
sleepEjin <jinking0930@naver.com>
2025-11-27 11:55:56 +0900

    11/27 제거

--- 06_React/react-project/src/main.jsx
+++ 06_React/react-project/src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  //strictMode -> 개발환경에서 문제를 파악하고 감지하기 위한 개발 전용 도구
-// <StrictMode>
+  <StrictMode>
     <App />
-// </StrictMode>,
+  </StrictMode>,
 )
